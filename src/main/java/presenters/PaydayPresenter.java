e6a3e2980ab2f20927d58f5208a70487cb2596b6
InariKitsune <sora.no.kitsune@gmail.com>
2017-05-29 10:11:54 +0900

    arreglados problemas con el merge

--- src/main/java/presenters/PaydayPresenter.java
+++ src/main/java/presenters/PaydayPresenter.java
@@ -1,20 +1,17 @@
 package presenters;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import payrollcasestudy.entities.PayCheck;
-import payrollcasestudy.transactions.PaydayTransaction;
-
-public class PaydayPresenter {
-	public PaydayPresenter()
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import payrollcasestudy.entities.PayCheck;
+import payrollcasestudy.transactions.PaydayTransaction;import payrollcasestudy.boundaries.*;
+public class PaydayPresenter {
+	public PaydayPresenter()
 	{}
-	public PayCheck getPayroll(int year, int month, int day, int employeeId)
-	{
-		Calendar payDate = new GregorianCalendar(year, month - 1, day);
-        PaydayTransaction paydayTransaction = new PaydayTransaction(payDate);
-        paydayTransaction.execute(PayrollDatabaseOnMemory.globalPayrollDatabase);
-        
-        PayCheck payCheck = paydayTransaction.getPaycheck(employeeId);
-        
-		return payCheck;
-	}
+	public PayCheck getPayroll(int year, int month, int day, int employeeId)
+	{
+		Calendar payDate = new GregorianCalendar(year, month - 1, day);
+        PaydayTransaction paydayTransaction = new PaydayTransaction(payDate);
+        paydayTransaction.execute(PayrollDatabaseOnMemory.globalPayrollDatabase);
+        PayCheck payCheck = paydayTransaction.getPaycheck(employeeId);   
+		return payCheck;
+	}
 }
--- src/main/java/presenters/TimeCardPresenter.java
+++ src/main/java/presenters/TimeCardPresenter.java
@@ -1,30 +1,27 @@
 package presenters;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import payrollcasestudy.boundaries.PayrollDatabase;
-import payrollcasestudy.entities.Employee;
-import payrollcasestudy.entities.TimeCard;
-import payrollcasestudy.entities.paymentclassifications.HourlyPaymentClassification;
-import payrollcasestudy.entities.paymentclassifications.PaymentClassification;
-import payrollcasestudy.transactions.Transaction;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import payrollcasestudy.boundaries.*;
+import payrollcasestudy.entities.Employee;
+import payrollcasestudy.entities.TimeCard;
+import payrollcasestudy.entities.paymentclassifications.HourlyPaymentClassification;
+import payrollcasestudy.entities.paymentclassifications.PaymentClassification;
+import payrollcasestudy.transactions.Transaction;
 import payrollcasestudy.transactions.add.*;
-
-public class TimeCardPresenter {
-	public TimeCardPresenter()
+public class TimeCardPresenter {
+	public TimeCardPresenter()
 	{}
-	public void addTimeCard(int employeeId, int year, int month, int day, int hours)
-	{
-		Calendar date = new GregorianCalendar(year, month - 1, day);
-        Transaction timeCardTransaction = new AddTimeCardTransaction(date, hours, employeeId);
-        timeCardTransaction.execute(PayrollDatabaseOnMemory.globalPayrollDatabase);
-	}
-	
-	public TimeCard[] getTimeCardArray(int employeeId)
-	{
-		Employee employee = PayrollDatabase.globalPayrollDatabase.getEmployee(employeeId);
-		PaymentClassification paymentClass = employee.getPaymentClassification();
-		if(paymentClass instanceof HourlyPaymentClassification)		
-			return ((HourlyPaymentClassification) paymentClass).getAllTimeCards();		
-		return null;
+	public void addTimeCard(int employeeId, int year, int month, int day, int hours)
+	{
+		Calendar date = new GregorianCalendar(year, month - 1, day);
+        Transaction timeCardTransaction = new AddTimeCardTransaction(date, hours, employeeId);
+        timeCardTransaction.execute(PayrollDatabaseOnMemory.globalPayrollDatabase);
+	}	
+	public TimeCard[] getTimeCardArray(int employeeId)
+	{		Employee employee = PayrollDatabaseOnMemory.globalPayrollDatabase.getEmployee(employeeId);
+		PaymentClassification paymentClass = employee.getPaymentClassification();
+		if(paymentClass instanceof HourlyPaymentClassification)	
+			return ((HourlyPaymentClassification) paymentClass).getAllTimeCards();	
+		return null;
 	}
 }
